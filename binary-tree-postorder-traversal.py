a3f87e39929d9afb2d8c45b89c0fcd9299e32566
lwoiceshyn <lwoiceshyn@gmail.com>
2020-01-31 17:24:10 -0600

    Improved detail of comments.

--- binary-tree-postorder-traversal.py
+++ binary-tree-postorder-traversal.py
@@ -5,11 +5,23 @@ https://leetcode.com/problems/binary-tree-postorder-traversal/
 class Solution:
     '''
     If you analyze the pattern of the postorder traversal, you'll notice that the reverse of the postorder
-    is actually the reverse preorder traversal (root right left) in reverse. Simply use a stack for the 
-    iterative reverse preorder traversal, and then reverse the the output.
+    is actually the reverse preorder traversal (root right left). Simply use a stack for the 
+    iterative reverse preorder traversal, and then reverse the the output. For iterative approach,
+    append the left before the right to the stack so the right gets processed before left.
 
     Time Complexity: O(n)
     Space Complexity: O(h)
+    
+    Example:
+            6
+           /  \
+          3    7
+         / |  / \
+        2  1 4   5
+      
+    Preorder: 6 3 2 1 7 4 5
+    Postorder: 2 1 3 4 5 7 6
+    Reverse Postorder: 6 7 5 4 3 1 2  (As if we we're doing Preorder but with left and right swapped)
     '''
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         stack = [root]
--- campus-bikes.py
+++ campus-bikes.py
@@ -1,6 +1,8 @@
 '''
 https://www.leetcode.com/campus-bikes/ (Premium)
 
+Problem Description:
+
 On a campus represented as a 2D grid, there are N workers and M bikes, with N <= M. Each worker 
 and bike is located on a coordinate on this grid.
 
@@ -23,25 +25,34 @@ Output: [1,0]
 
 Note: 1 <= N <= M <= 1000 (There is at least 1 worker, 1 bike, and no more than 1000 of each for any input case)
 
-Thought Process:
-Brute Force: Go through every worker, bike pair, compute the distance, and store those values,
--We now have a data structure mapping every pair of worker/bike to a distance value.
--Sort the distance values in increasing order.
--start making assignments by iterating through the distance values, and removing workers 
-once an assignment has been made to them. settle disputes as discussed above.
--convert the worker:bike assignments structure to the desired output list
 
-O(m*n)
+Solution:
+
+Go through every worker, and for each worker, check every bike. Create a mapping which stores the L1 distance
+to each bike for each worker, in descending order, such that the shortest distance bike can be popped from the
+end of the list. Store the mapping as a list of nested tuples, where each tuple follows (distance, worker, bike),
+for tuple comparison purposes later.
+
+Then, create a min heap of size(num_workers), which will store the current bicycle which is the shortest distance to
+each worker, from shortest to longest. Also create a set to keep track of which bikes have already been assigned to
+a worker. Then, check while the size of the set is still less than the number of workers (not every worker has 
+been assigned a bike yet), pop the min value from the heap (sorted by distance first, then worker ID, then bike ID
+due to the tuple representation). Check if the bike is in the set already. If not, assign the bike to worker, and add
+it to the set. If so, get the next best bike in terms of distance for that worker and push it into the heap.
+
+Time Complexity: O(M*N), where M and N are the number of workers and bikes.
+Space Complexity: O(M), since all of the data structures used are linear in 
+the number of total workers.
 '''
 class Solution:
    def assignBikes(self, workers, bikes):
-        distances = []     # distances[worker] is tuple of (distance, worker, bike) for each bike 
+        distances = []     
         for i, (x, y) in enumerate(workers):
             distances.append([])
             for j, (x_b, y_b) in enumerate(bikes):
                 distance = abs(x - x_b) + abs(y - y_b)
                 distances[-1].append((distance, i, j))
-            distances[-1].sort(reverse = True)  # reverse so we can pop the smallest distance
+            distances[-1].sort(reverse = True) 
 
         result = [None] * len(workers)
         used_bikes = set()
